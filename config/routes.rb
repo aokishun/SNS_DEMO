7b25ee9a3f6bd6a6c07521c01f736afd32e0c0b4
りんご <aochaaaaannnn@aokishunnoMacBook-ea.local>
2018-05-20 15:08:14 +0900

    Finish user follow / unfollow actions

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,13 @@ Rails.application.routes.draw do
     devise_for :users, :controllers => {
       :registrations => "registrations"
     }
-    resources :users, only: [:show, :index, :destroy]
+    resources :users, only: [:show, :index, :destroy] do
+      member do
+        get :following, :followers
+      end
+    end
     resources :microposts, only: [:create, :destroy]
+    resources :relationships, only: [:create, :destroy]
 
   root 'home#home'
   get 'help' => 'home#help'
